8f67e241328fdb8a76c280eb68368ce298bc2c1f
Mark <mark@remarkablemark.org>
2025-08-24 20:10:45 -0400

    chore(main): ad player movement

--- main.lua
+++ main.lua
@@ -1,9 +1,29 @@
 function love.load()
+    player = {}
+    player.x = 0
+    player.y = 0
+    player.speed = 3
 end
 
 function love.update(dt)
+    if love.keyboard.isDown("right") then
+        player.x = player.x + player.speed
+    end
+
+    if love.keyboard.isDown("left") then
+        player.x = player.x - player.speed
+    end
+
+    if love.keyboard.isDown("down") then
+        player.y = player.y + player.speed
+    end
+
+    if love.keyboard.isDown("up") then
+        player.y = player.y - player.speed
+    end
 end
 
 function love.draw()
     love.graphics.print("Hello World", 400, 300)
+    love.graphics.circle("fill", player.x, player.y, 100)
 end
